1441c40632050accb57f41a1129d3d9ae9768e8d
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-02-03 18:05:37 -0500

    review pep8

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,23 +7,20 @@ import requests
 from sys import argv
 
 
-def export(user_id):
-    '''
-        scrip python for export date format Json
-    '''
+if __name__ == "__main__":
+
     userid = argv[1]
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                         .format(userid)).json()
     All = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                        .format(userid)).json()
+                       .format(userid)).json()
     N = user.get('name')
     filename = userid + '.csv'
     with open(filename, 'w', newline='') as csv_file:
         tasks = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in All:
             tasks.writerow([userid, user.get('username'),
-                           task.get('completed'), task.get('title')])
+                            task.get('completed'), task.get('title')])
 
 
 if __name__ == "__main__":
-    export(int(argv[1]))
