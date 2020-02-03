056b7c9ee7d8aa0df83f8211f800e510b153296d
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-02-03 17:05:40 -0500

    export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    Export a JSON
+'''
+import csv
+import requests
+from sys import argv
+
+
+def export(user_id):
+    '''
+        scrip python for export date format Json
+    '''
+    userid = argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userid)).json()
+    All = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(userid)).json()
+    N = user.get('name')
+    filename = userid + '.csv'
+    with open(filename, 'w', newline='') as csv_file:
+        tasks = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in All:
+            tasks.writerow([userid, user.get('username'),
+                           task.get('completed'), task.get('title')])
+
+
+if __name__ == "__main__":
+    export(int(argv[1]))
