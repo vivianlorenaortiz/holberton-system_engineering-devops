23077525abab3ae0f0fc75fefb8b1dd8c3db4821
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-02-06 20:35:55 -0500

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """ function that queries the Reddit API and prints the titles """
 
 import requests
@@ -9,12 +9,13 @@ def top_ten(subreddit):
     """
     """
     try:
-        url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                             .format(subreddit),
-                             headers={'User-agent': 'vivianortiz'},
-                             allow_redirects=False).json()
+        res = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                           .format(subreddit),
+                           headers={'User-agent': 'vivianortiz'},
+                           allow_redirects=False).json()
 
-        for top in url.get('data').get('children'):
-            print(top.get('data').get('title'))
-    except Exception:
+        for top in range(0, 10):
+            print(res.json().get('data').get('children')[top]
+                  .get('data', {}).get('title'))
+    except:
         print(None)
