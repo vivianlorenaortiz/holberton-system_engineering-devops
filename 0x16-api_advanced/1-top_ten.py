e9c4922f28564dd3387377550d32f18468d4c8dd
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-02-06 20:40:13 -0500

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,15 @@ import requests
 from sys import argv
 
 
-def top_ten(subreddit):
-    """
-    """
-    try:
-        res = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                           .format(subreddit),
-                           headers={'User-agent': 'vivianortiz'},
-                           allow_redirects=False).json()
 
-        for top in range(0, 10):
-            print(res.json().get('data').get('children')[top]
+def top_ten(subreddit):
+    try:
+        res = requests.get("https://www.reddit.com/r/{}/hot/.json?limit=10"
+                           .format(subreddit),
+                           headers={'User-agent': 'Holberton'},
+                           allow_redirects=False)
+        for i in range(0, 10):
+            print(res.json().get("data").get("children")[i]
                   .get('data', {}).get('title'))
     except:
         print(None)
